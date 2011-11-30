22db735f26fc10ceada276908bdd6aea32369c23
Miguel Vazquez <miguel.vazquez@fdi.ucm.es>
2011-11-30 17:47:01 +0100

    nil

--- lib/rbbt/entity/misc.rb
+++ lib/rbbt/entity/misc.rb
@@ -3,24 +3,3 @@ require 'rbbt/workflow'
 require 'rbbt/sources/go'
 require 'rbbt/sources/organism'
 require 'rbbt/entity/gene'
-
-module GOTerm
-  extend Entity
-  self.annotation :organism
-
-  self.format = ["GO Term", "GO ID"]
-
-  def name
-    if Array === self
-      self.collect{|id| GO.id2name(id)}
-    else
-      GO.id2name(self)
-    end
-  end
-
-  def genes
-    go2genes = Organism.gene_go(organism).tsv(:key_field => "GO ID", :fields => ["Ensembl Gene ID"], :merge => true, :persist => true)
-    go2genes.unnamed = true
-    Gene.setup(go2genes[self].first, "Ensembl Gene ID", organism)
-  end
-end
