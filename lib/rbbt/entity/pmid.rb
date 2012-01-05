bcc7e3928fe6a105914d73be1cd76ce353259a63
Miguel Vazquez <miguel.vazquez@cnio.es>
2012-01-05 18:25:44 +0100

    nil

--- lib/rbbt/entity/pmid.rb
+++ lib/rbbt/entity/pmid.rb
@@ -6,11 +6,24 @@ module PMID
 
   self.format = "PMID"
 
+  property :article => :array2single do
+    @article ||= begin
+                   PubMed.get_article(self).values_at(*self)
+                 end
+  end
+
   property :title => :array2single do
     @title ||= begin
-                 PubMed.get_article(self).values_at(*self).collect{|article| article.nil? ? nil : article.title}
+                 article.collect{|a| a.nil? ? nil : a.title}
                end
   end
+
+  property :text => :array2single do
+    @text ||= begin
+                 article.collect{|a| a.nil? ? nil : a.text}
+               end
+  end
+
 
   property :pubmed_url => :single2array do
     "<a class='pmid' href='http://www.ncbi.nlm.nih.gov/pubmed/#{self}'>#{ self }</a>"
