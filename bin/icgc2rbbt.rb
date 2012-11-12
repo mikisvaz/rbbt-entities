f2d87fef7b69cfee183e8f966da236549ad193e0
Miguel Vazquez <miguel.vazquez@cnio.es>
2012-11-12 19:20:18 +0100

    nil

---
+++ bin/icgc2rbbt.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+require 'rbbt/util/open'
+
+file = ARGV.shift
+directory = ARGV.shift
+
+genotypes = {}
+Open.read(file).split("\n").each do |line|
+  next if line =~ /^Cancer Type/
+
+  chr, pos, ref, mut, sample = line.split(/\t/).values_at 2, 3, 6, 10, 28 
+
+  chr.sub!(/chr/,'')
+  mut = mut + '-' * (ref.length - mut.length) if ref.length > mut.length
+
+  genotypes[sample] ||= []
+  genotypes[sample] << [chr, pos, mut] * ":"
+end
+
+genotypes.each do |sample, mutations|
+  mutations.uniq!
+  Open.write(File.join(directory, sample), mutations.uniq * "\n")
+end
