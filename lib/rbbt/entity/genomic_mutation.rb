fc10d111da64d2b8f6dc4a7d94f5bc758fbf0e31
Miguel Vazquez <miguel.vazquez@cnio.es>
2012-11-16 03:24:19 +0100

    nil

--- lib/rbbt/entity/genomic_mutation.rb
+++ lib/rbbt/entity/genomic_mutation.rb
@@ -204,7 +204,7 @@ module GenomicMutation
 
   property :affected_genes => :array2single do
     Gene.setup(mutated_isoforms.collect{|mis|
-      genes = mis.nil? ? [] : mis.protein.gene
+      genes = mis.nil? ? [] : mis.protein.gene.compact.uniq
       Gene.setup(genes, "Ensembl Gene ID", organism)
     }, "Ensembl Gene ID", organism)
   end
@@ -213,7 +213,7 @@ module GenomicMutation
     _mutated_isoforms = mutated_isoforms
     mi_damaged = Misc.process_to_hash(MutatedIsoform.setup(_mutated_isoforms.compact.flatten.uniq, organism)){|mis| mis.damaged?(*args)}
     Gene.setup(_mutated_isoforms.select{|mi| mi_damaged[mi]}.collect{|mis|
-      genes = mis.nil? ? [] : mis.protein.gene
+      genes = mis.nil? ? [] : mis.protein.gene.compact.uniq
       Gene.setup(genes, "Ensembl Gene ID", organism)
     }, "Ensembl Gene ID", organism)
   end
